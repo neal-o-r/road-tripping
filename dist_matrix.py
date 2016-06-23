e32f1d66dd314570c7e142f74813e1ec760ee19e
neal-o-r <neal@n-o-r.xyz>
2016-06-23 19:43:30 +0100

    sync

--- dist_matrix.py
+++ dist_matrix.py
@@ -1,3 +1,4 @@
+import wikipedia
 import googlemaps
 import numpy as np
 import seaborn as sns; sns.set()
@@ -12,39 +13,16 @@ f.close()
 urls = [url.rstrip('\n') for url in urls]
 
 names = []
-
+latlon = []
 for url in urls:
-
+	
 	url = (url.split('/')[-1]).replace('%27', "'")
-	names.append( url.replace('_', "+") )
-
-
-f = open('api.key','r')
-
-key = f.read()
-key = key.rstrip(' \n')
-f.close()
-
-gcl = googlemaps.Client(key=key)
-
-dist_mat = np.zeros((len(names), len(names)))
-
-for i, name1 in enumerate(names):
-
-	print i / float(len(names))
-
-	for j, name2 in enumerate(names[i:]):
-
-		if name1 == name2:
-			dist_mat[i][j] = 0.
-
-		try:
-			response = googlemaps.distance_matrix.distance_matrix(
-                            gcl, name1, name2, mode='driving',language='English', units='metric')
-		
-			dist_mat[i][i+j] = response["rows"][0]["elements"][0]["distance"]["value"]
-		except Exception:
-			print("No route between %s and %s" %(name1, name2))
-
-np.savetxt("output.txt", dist_mat)
-
+	name = url.replace('_', " ") 
+	names.append(name)
+	a = wikipedia.page(name)
+	try:
+		latlon.append((a.coordinates))
+	except:
+		latlon.append(None)
+		continue 
+	print name
